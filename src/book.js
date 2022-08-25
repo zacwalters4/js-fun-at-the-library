8d8dd9a1b56278c55e71156bd9263013bbc63ccb
Zac Walters <zacwalters4@gmail.com>
2022-08-25 10:31:19 -0600

    Complete remaining book-tests.js

--- src/book.js
+++ src/book.js
@@ -18,22 +18,34 @@ var isUnique = true
 for(i = 0; i < reviews.length; i++){
   if(comment === reviews[i]){
     isUnique = false
-
     }
   }
 if(isUnique === false){
-
   }
   else{
     reviews.push(comment)
   }
-
 return(reviews)
 }
 
-var test = []
+function calculatePageCount(title){
+  return title.length * 20
+}
 
+function writeBook(title, character, genre){
+  var book = {
+    title:  title,
+    mainCharacter:  character,
+    pageCount:  calculatePageCount(title),
+    genre:  genre,
+  }
+  return book
+}
 
+function editBook(book){
+  book.pageCount = book.pageCount * 3 / 4
+  return book
+}
 
 
 
@@ -41,7 +53,7 @@ module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-//   editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
