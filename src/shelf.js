318616a985c66b615c8382127d5ff772a30ade49
Zac Walters <zacwalters4@gmail.com>
2022-08-25 13:49:40 -0600

    Pass all shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,54 @@
+function shelfBook(book, sciFiShelf){
+if(sciFiShelf.length < 3){
+  sciFiShelf.unshift(book)
+}
+  return sciFiShelf
+}
+
+function unshelfBook(book, sciFiShelf){
+
+for(i = 0; i < sciFiShelf.length; i++){
+//  console.log(sciFiShelf[i])
+  if(sciFiShelf[i].title === book){
+  sciFiShelf.splice(i, 1)
+
+}
+}
+  return sciFiShelf
+
+}
+
+function listTitles(shelf){
+  var titles = ""
+  for(i = 0; i < shelf.length; i++){
+    if(i === 0){
+      titles += shelf[i].title
+    }
+    else {
+      titles += ", " + shelf[i].title
+    }
+  }
+
+return titles
+}
+
+function searchShelf(shelf, title){
+var onShelf = false
+for(i=0; i<shelf.length; i++){
+
+  if(shelf[i].title === title){
+    onShelf = true
+  }
+}
+    return onShelf
+
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+   unshelfBook,
+   listTitles,
+   searchShelf
 };
