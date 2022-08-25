e299e6376930d304f89507e869a1995e9e796c0e
Zac Walters <zacwalters4@gmail.com>
2022-08-25 09:46:02 -0600

    Complete buildMainCharacter and saveReview checklist

--- src/book.js
+++ src/book.js
@@ -3,11 +3,45 @@ function createTitle(title) {
   return title
 }
 
+function buildMainCharacter(name, age, pronouns){
+mainCharacter = {
+  name: name,
+  age: age,
+  pronouns: pronouns
+}
+return mainCharacter
+}
+
+function saveReview(comment, reviews){
+var isUnique = true
+
+for(i = 0; i < reviews.length; i++){
+  if(comment === reviews[i]){
+    isUnique = false
+
+    }
+  }
+if(isUnique === false){
+
+  }
+  else{
+    reviews.push(comment)
+  }
+
+return(reviews)
+}
+
+var test = []
+
+
+
+
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
-  // editBook
+//   editBook
 }
